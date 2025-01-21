21f9431290843d2c18734e1ca3d29239b04024b6
s464868 <adaszp@st.amu.edu.pl>
2025-01-21 23:32:36 +0100

    Add embedding model evaluation and update README.md by adding experiment conclusions

--- process_data.py
+++ process_data.py
@@ -1,11 +1,14 @@
 import json
 import uuid
+import time
 
 from qdrant_client.http.models import ScoredPoint
+from sentence_transformers import SentenceTransformer
 
-from chunking import ChunkingProcessor, FixedSizeChunking, FullSentenceChunking
+from chunking import ChunkingProcessor, FullSentenceChunking
 from constants import METRIC_DOT, METRIC_COSINE, METRIC_EUCLID, METRIC_MANHATTAN, BASE_COLLECTION_NAME, CONTAINER_PORT, \
-    CONTAINER_URL, MODEL_NAME
+    CONTAINER_URL, MODEL_NAME_ALL_MINILM, MODEL_NAME_PARAPHRASE_MINILM, MODEL_NAME_DISTILBERT, MODEL_NAME_MPNET, \
+    MODEL_NAME_DISTILROBERTA
 from qdrant_manager import QdrantManager
 from questions import ai_questions
 from readers import DocumentReader, get_document_reader, Document
@@ -56,14 +59,24 @@ def article_parser(articles: Document):
     return documents, child_documents
 
 
-def process_articles(qdrant_manager_instance: QdrantManager):
+def process_articles(qdrant_manager_instance: QdrantManager, vector_size: int):
     processed_articles, processed_pages = read_articles()
-    print(processed_articles[0])
-    print(processed_pages[0])
 
-    add_base_articles_to_qdrant(qdrant_manager_instance=qdrant_manager_instance, documents=processed_articles)
-    add_page_chunked_articles_to_qdrant(qdrant_manager_instance=qdrant_manager_instance, documents=processed_pages)
+    add_base_articles_to_qdrant(qdrant_manager_instance=qdrant_manager_instance, documents=processed_articles,
+                                base_collection_name=BASE_COLLECTION_NAME, vector_size=vector_size)
+    add_page_chunked_articles_to_qdrant(qdrant_manager_instance=qdrant_manager_instance, documents=processed_pages,
+                                        base_collection_name=BASE_COLLECTION_NAME, vector_size=vector_size)
     # add_sentence_chunked_articles_to_qdrant(qdrant_manager_instance=qdrant_manager_instance, documents=processed_pages)
+
+
+def process_articles_for_embedding_experiment(qdrant_manager_instance: QdrantManager, base_collection_name: str,
+                                              vector_size: int):
+    processed_articles, processed_pages = read_articles()
+
+    add_base_articles_to_qdrant(qdrant_manager_instance=qdrant_manager_instance, documents=processed_articles,
+                                base_collection_name=base_collection_name, vector_size=vector_size)
+    add_page_chunked_articles_to_qdrant(qdrant_manager_instance=qdrant_manager_instance, documents=processed_pages,
+                                        base_collection_name=base_collection_name, vector_size=vector_size)
 
 
 def read_articles():
@@ -80,10 +93,10 @@ def read_articles():
 
 
 def create_collection_and_add_documents(qdrant_manager_instance: QdrantManager, collection_name: str,
-                                        documents: [Document], metric: str):
+                                        documents: [Document], metric: str, vector_size: int):
     print(f"Adding documents to {collection_name}")
     qdrant_manager_instance.delete_collection(collection_name)
-    qdrant_manager_instance.create_collection(collection_name, distance_metric=metric)
+    qdrant_manager_instance.create_collection(collection_name, distance_metric=metric, vector_size=vector_size)
 
     for document in documents:
         qdrant_manager_instance.add_document(document, collection_name)
@@ -104,20 +117,32 @@ def apply_sentence_chunking_to_documents(documents: [Document]):
     return result
 
 
-def add_base_articles_to_qdrant(qdrant_manager_instance: QdrantManager, documents: [Document]):
-    for metric in ALL_METRICS:
-        article_collection_name = f'{BASE_COLLECTION_NAME}s_{metric.lower()}'
-        create_collection_and_add_documents(qdrant_manager_instance, article_collection_name,
-                                            documents=documents, metric=metric)
+def add_base_articles_to_qdrant(qdrant_manager_instance: QdrantManager, documents: [Document],
+                                base_collection_name: str, vector_size: int):
+    metric = METRIC_COSINE
+
+    article_collection_name = f'{base_collection_name}_{metric.lower()}'
+    create_collection_and_add_documents(qdrant_manager_instance=qdrant_manager_instance,
+                                        collection_name=article_collection_name,
+                                        documents=documents,
+                                        metric=metric,
+                                        vector_size=vector_size)
 
 
-def add_page_chunked_articles_to_qdrant(qdrant_manager_instance: QdrantManager, documents: [Document]):
-    for metric in ALL_METRICS:
-        article_pages_collection_name = f'{BASE_COLLECTION_NAME}_pages_{metric.lower()}'
-        create_collection_and_add_documents(qdrant_manager_instance, article_pages_collection_name,
-                                            documents=documents, metric=metric)
+def add_page_chunked_articles_to_qdrant(qdrant_manager_instance: QdrantManager, documents: [Document],
+                                        base_collection_name: str, vector_size: int):
+    metric = METRIC_COSINE
 
-def add_sentence_chunked_articles_to_qdrant(qdrant_manager_instance: QdrantManager, documents: [Document]):
+    article_pages_collection_name = f'{base_collection_name}_pages_{metric.lower()}'
+    create_collection_and_add_documents(qdrant_manager_instance=qdrant_manager_instance,
+                                        collection_name=article_pages_collection_name,
+                                        documents=documents,
+                                        metric=metric,
+                                        vector_size=vector_size)
+
+
+def add_sentence_chunked_articles_to_qdrant(qdrant_manager_instance: QdrantManager, documents: [Document],
+                                            vector_size: int):
     documents_sentence_chunked = apply_sentence_chunking_to_documents(documents)
 
     articles_chunked_collection_name = 'articles_sentence_chunking'
@@ -125,7 +150,9 @@ def add_sentence_chunked_articles_to_qdrant(qdrant_manager_instance: QdrantManag
     create_collection_and_add_documents(qdrant_manager_instance=qdrant_manager_instance,
                                         collection_name=articles_chunked_collection_name,
                                         documents=documents_sentence_chunked,
-                                        metric=METRIC_COSINE)
+                                        metric=METRIC_COSINE,
+                                        vector_size=vector_size)
+
 
 def distance_experiment(qdrant_manager_instance: QdrantManager):
     collection_names = [collection.name for collection in qdrant_manager_instance.get_client_collections().collections]
@@ -135,8 +162,7 @@ def distance_experiment(qdrant_manager_instance: QdrantManager):
     for collection_name in collection_names:
         scores = []
         for question in ai_questions:
-            scoredPoint: ScoredPoint = qdrant_manager.search(collection_name=collection_name, query=question, top_k=1)[
-                0]
+            scoredPoint: ScoredPoint = qdrant_manager_instance.search(collection_name=collection_name, query=question, top_k=1)[0]
             scores.append(scoredPoint.score)
 
         distance_metric_results[collection_name] = sum(scores) / len(scores)
@@ -144,9 +170,81 @@ def distance_experiment(qdrant_manager_instance: QdrantManager):
     print(distance_metric_results)
 
 
+def model_embeddings_experiment(model_names: [str]):
+    model_results = {}
+    for model_name in model_names:
+
+        only_model_name = model_name.split('/')[-1]
+
+        print(f"Testing embeddings for model: {only_model_name}")
+
+
+        print(f"Creating embedding model: {only_model_name}")
+        embedding_model = SentenceTransformer(model_name, cache_folder='./model_cache')
+        embedding_model_vector_size = embedding_model.get_sentence_embedding_dimension()
+        print(f"Embedding model {only_model_name} created")
+
+        qdrant_manager = QdrantManager(CONTAINER_URL, CONTAINER_PORT, embedding_model)
+
+        base_collection_name = f"articles_{only_model_name}"
+
+        print(f"Processing articles for embedding model: {only_model_name}")
+        process_articles_for_embedding_experiment(qdrant_manager_instance=qdrant_manager, vector_size=embedding_model_vector_size, base_collection_name=base_collection_name)
+        print(f"Processing articles for embedding model {only_model_name} finished")
+
+        collection_name_articles = f'{base_collection_name}_{METRIC_COSINE.lower()}'
+        collection_name_article_pages = f'{base_collection_name}_pages_{METRIC_COSINE.lower()}'
+
+        print(f"Testing embedding model: {only_model_name}")
+        articles_avg_score, articles_avg_time = test_qdrant_model_embeddings(qdrant_manager_instance=qdrant_manager, collection_name=collection_name_articles)
+        article_pages_avg_score, article_pages_avg_time = test_qdrant_model_embeddings(qdrant_manager_instance=qdrant_manager, collection_name=collection_name_article_pages)
+        print(f"Testing embedding model {only_model_name} finished")
+
+        model_results[collection_name_articles] = {'avg_score': articles_avg_score, 'avg_time': articles_avg_time}
+        model_results[collection_name_article_pages] = {'avg_score': article_pages_avg_score, 'avg_time': article_pages_avg_time}
+        print(model_results[collection_name_articles])
+        print(model_results[collection_name_article_pages])
+
+
+    print(model_results)
+
+
+def test_qdrant_model_embeddings(qdrant_manager_instance: QdrantManager, collection_name: str):
+
+    scores = []
+    times = []
+    for question in ai_questions:
+        start_time = time.perf_counter()
+
+        scoredPoint: ScoredPoint = qdrant_manager_instance.search(collection_name=collection_name, query=question, top_k=1)[0]
+
+        end_time = time.perf_counter()
+
+        scores.append(scoredPoint.score)
+        times.append(end_time - start_time)
+
+
+    avg_score = sum(scores) / len(scores)
+    avg_time = sum(times) / len(times)
+
+    return avg_score, avg_time
+
 if __name__ == '__main__':
-    qdrant_manager = QdrantManager(CONTAINER_URL, CONTAINER_PORT, MODEL_NAME)
+    # model_names = [
+    #     MODEL_NAME_ALL_MINILM,
+    #     MODEL_NAME_PARAPHRASE_MINILM,
+    #     MODEL_NAME_DISTILBERT,
+    #     MODEL_NAME_MPNET,
+    #     MODEL_NAME_DISTILROBERTA
+    # ]
+    #
+    # model_embeddings_experiment(model_names=model_names)
 
-    process_articles(qdrant_manager)
+    # Best model based on experiment
+    embedding_model = SentenceTransformer(MODEL_NAME_PARAPHRASE_MINILM, cache_folder='./model_cache')
+    embedding_model_vector_size = embedding_model.get_sentence_embedding_dimension()
 
-    distance_experiment(qdrant_manager)
+    qdrant_manager = QdrantManager(CONTAINER_URL, CONTAINER_PORT, embedding_model)
+
+    process_articles(qdrant_manager_instance=qdrant_manager, vector_size=embedding_model_vector_size)
+
